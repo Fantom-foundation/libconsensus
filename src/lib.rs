bd8b645a4d1737197b85a546ed3e72157b48daaa
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-05-02 18:14:18 +1000

    explicitly specify callback fn type as parameter of register_callback()

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,5 @@
 
 pub trait Consensus<B: AsRef<u8>> {
-    // Finalised transaction callback function type.
-    //
-    // The callback function must accept only one parameter of type B; and
-    // must return True when transaction is processed successfully and False otherwise.
-    // The callback function will be called with the same transaction until
-    // callback function returns True; a pause between  consecutive calls of the
-    // callback function with the same block will be made for the value of milliseconds
-    // set by callback_timeout() function of the Consensus trait below;
-    // default value of the timeout is implementation defined.
-    //
-    // Example callback type:
-    // type Callback<B: AsRef<u8>> = fn(data: B) -> bool;
-    type Callback;
-
     // Consensus configuration type
     type Configuration;
 
@@ -34,7 +20,13 @@ pub trait Consensus<B: AsRef<u8>> {
     // It returns True on successful registration and False otherwise
     // Several callback function can be registered, they will be called in
     // the order of registration.
-    fn register_callback (&mut self, callback: Self::Callback) -> bool;
+    fn register_callback (&mut self, callback: fn(data: B)->bool) -> bool;
+    // The callback function must return True when transaction is processed successfully and False otherwise.
+    // The callback function will be called with the same transaction until
+    // callback function returns True; a pause between  consecutive calls of the
+    // callback function with the same block will be made for the value of milliseconds
+    // set by callback_timeout() function of the Consensus trait below;
+    // default value of the timeout is implementation defined.
 
     // Set timeout in milliseconds between consecutive calls of the callback
     // function with the same transaction.
