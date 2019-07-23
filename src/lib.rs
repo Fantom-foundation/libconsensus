69991af07de6610c924df23579a76048ab3ef5d0
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-07-23 14:02:07 +1000

    src/lib.rs: rename B parameter type into Data

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,8 @@ pub enum TransactionType {
 pub trait Consensus {
     // Consensus configuration type
     type Configuration;
-    // Consensus data type; specify data type of event to be in consensus on order of.
-    type B: AsRef<u8>;
+    // Consensus data type; specify data type of events to be in consensus on order of.
+    type Data: AsRef<u8>;
 
     // Create new Consensus instance
     fn new(cfg: Self::Configuration) -> Self;
@@ -34,14 +34,14 @@ pub trait Consensus {
     fn shutdown();
 
     // Send a transaction into Consensus
-    fn send_transaction(&mut self, data: Self::B);
+    fn send_transaction(&mut self, data: Self::Data);
 
     // Register a callback function which is called when a transaction
     // is finalised in the consensus.
     // It returns True on successful registration and False otherwise
     // Several callback function can be registered, they will be called in
     // the order of registration.
-    fn register_callback(&mut self, callback: fn(data: Self::B) -> bool) -> bool;
+    fn register_callback(&mut self, callback: fn(data: Self::Data) -> bool) -> bool;
     // The callback function must return True when transaction is processed successfully and False otherwise.
     // The callback function will be called with the same transaction until
     // callback function returns True; a pause between  consecutive calls of the
@@ -58,7 +58,7 @@ pub trait Consensus {
     // It returns True on successful registration and False otherwise
     // Several channels can be registered, they will be pushed in
     // the order of registration.
-    fn register_channel(&mut self, sender: Sender<Self::B>) -> bool;
+    fn register_channel(&mut self, sender: Sender<Self::Data>) -> bool;
 
     // Register a PipeWriter of os_pipe::pipe; which is used to push
     // all finalised transaction to.
