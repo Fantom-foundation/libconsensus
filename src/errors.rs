309f9e682dc271125b0c78275eaf8e1e0b0711fb
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-10-01 06:20:28 -0700

    Use failure
    I decided to temporarily leave the libcommon::Error code here to make
    the transition backwards compatible and not break other people's code.

--- src/errors.rs
+++ src/errors.rs
@@ -1,21 +1,25 @@
 /// # Fantom/libconsensus/errors
 ///
 /// This file defines a set of errors which are used within the consensus traits.
+use failure::{Error as FailureError};
 use libcommon_rs::errors::Error as BaseError;
 use libtransport::errors::Error as TransportError;
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = std::result::Result<T, FailureError>;
 
 /// Differentiates between multiple error types. Errors are outlined as follows:
 /// Base: A base error, as defined in the libcommon crate.
 /// AtMaxVecCapacity: Returned when a new element is added to a vector of full capacity.
 /// Transport: An error caused by a transport protocol error.
-#[derive(Debug)]
+#[derive(Debug, Fail)]
 pub enum Error {
+    #[fail(display = "Base Error: {:?}", 0)]
     Base(BaseError),
     // Error indicating Vec<T> is reached maximum capacity and would cause
     // panic while adding next element.
+    #[fail(display = "Internal vector is at maximum capacity!")]
     AtMaxVecCapacity,
+    #[fail(display = "Transport Error: {:?}", 0)]
     Transport(TransportError),
 }
 
