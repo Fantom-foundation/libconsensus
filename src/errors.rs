22dd52a2ae7db50a59fb5b515d56f67d1d2e88a6
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-10-02 06:05:11 -0700

    Remove necessity of libcommon::Error

--- src/errors.rs
+++ src/errors.rs
@@ -2,7 +2,6 @@
 ///
 /// This file defines a set of errors which are used within the consensus traits.
 use failure::Error as FailureError;
-use libcommon_rs::errors::Error as BaseError;
 use libtransport::errors::Error as TransportError;
 
 pub type Result<T> = std::result::Result<T, FailureError>;
@@ -13,36 +12,10 @@ pub type Result<T> = std::result::Result<T, FailureError>;
 /// Transport: An error caused by a transport protocol error.
 #[derive(Debug, Fail)]
 pub enum Error {
-    #[fail(display = "Base Error: {:?}", 0)]
-    Base(BaseError),
     // Error indicating Vec<T> is reached maximum capacity and would cause
     // panic while adding next element.
     #[fail(display = "Internal vector is at maximum capacity!")]
     AtMaxVecCapacity,
     #[fail(display = "Transport Error: {:?}", 0)]
     Transport(TransportError),
-}
-
-/// Allow an error to be transformed into a BaseError.
-impl From<BaseError> for Error {
-    #[inline]
-    fn from(b: BaseError) -> Error {
-        Error::Base(b)
-    }
-}
-
-/// Allow an error to be transformed into a TransportError
-impl From<TransportError> for Error {
-    #[inline]
-    fn from(t: TransportError) -> Error {
-        Error::Transport(t)
-    }
-}
-
-/// What happens when a 'NONE' variant is unwrapped.
-#[macro_export]
-macro_rules! none_error {
-    () => {
-        libconsensus::errors::Error::Base(libcommon_rs::errors::Error::NoneError)
-    };
 }
